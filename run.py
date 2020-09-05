a36440ad53b8c633207408f0aff8d1f0c94fb2b6
GamerNoTitle <bili33@87ouo.top>
2020-09-05 13:32:12 +0800

    debug

--- run.py
+++ run.py
@@ -18,6 +18,6 @@ for i in range(0, len(urls)):
     req = requests.get(urls[i])
     print(f'第{i}号网址唤醒状态:', req, time.strftime(
         '%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-    message='[WakeLeancloud]第{i}号网址唤醒状态:', req, time.strftime(
-        '%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    message='[WakeLeancloud]第{i}号网址唤醒状态:'+req+time.strftime(
+        '%Y-%m-%d %H:%M:%S'+time.localtime(time.time()))
     send(message)
